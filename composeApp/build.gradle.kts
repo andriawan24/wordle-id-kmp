29d6b0eac152cc5e1bebc743f6dbcbaa133da606
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-02-19 22:18:40 +0700

    feature(clean): Clean up code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,16 +57,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
